9ac23136fd3c6dece8bc763d80b575e4b169a2b6
Tushar Arora <tushar.arora@ibm.com>
2022-01-05 14:56:17 +0530

    updating our code to be a pwa complaint, adding serviceworker, https, update manifest

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -13,8 +15,11 @@ const port = process.env.PORT || 5005;
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+    // app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', (req, res) => {
@@ -25,6 +30,10 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, err => {
     if (err) throw err;
     console.log('Server is running on ' + port);
+});
+
+app.use('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build','service-worker.js'));
 });
 
 app.post('/payment', (req, res) => {
