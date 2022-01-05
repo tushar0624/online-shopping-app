f4b3b163f76c5113163cd193812d50a7216399b5
Tushar Arora <tushar.arora@ibm.com>
2022-01-05 17:59:28 +0530

    disable serviceworker in server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
@@ -14,9 +15,10 @@ const port = process.env.PORT || 5005;
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', (req, res) => {
@@ -29,9 +31,9 @@ app.listen(port, err => {
     console.log('Server is running on ' + port);
 });
 
-app.use('/service-worker.js', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'build','service-worker.js'));
-});
+// app.use('/service-worker.js', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '..', 'build','service-worker.js'));
+// });
 
 app.post('/payment', (req, res) => {
     if (Object.keys(req.body).length > 1) {
