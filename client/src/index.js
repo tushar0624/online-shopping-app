70fd9b6b0dd07ef6f67efb36df29889bede27c8a
Tushar Arora <tushar.arora@ibm.com>
2022-01-06 13:21:06 +0530

    Added logs in index for check static path of serviceworker file

--- client/src/index.js
+++ client/src/index.js
@@ -21,4 +21,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+console.log(`First URL: ${process.env.PUBLIC_URL}/service-worker.js`);
+console.log(`Second Url: ${process.env.PUBLIC_URL}/static/service-worker.js`);
+
 serviceWorker.register();
